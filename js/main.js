ec6850329e47a559d3339551a91ad7b8973998bb
sajedul-dev <sajedulislamraja@gmail.com>
2023-07-27 20:33:56 +0600

    portfolio uploaded

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function(){
+    // botton to top btn operation
+
+    $(".bottom-to-top").click(function(){
+        $("body,html").animate({scrollTop:0},2000)
+    });
+
+    $(window).scroll(function(){
+        let scroll = $(this).scrollTop()
+        console.log(scroll);
+        if(scroll > 200){
+            $(".bottom-to-top").fadeIn(500)
+        }else{
+            $(".bottom-to-top").fadeOut(500)
+        }
+       
+        // fixed navbar operation
+
+        if(scroll > 120){
+            $(".nav").addClass("fixed")
+        }else{
+            $(".nav").removeClass("fixed") 
+        }
+        
+    })
+    // preloader operation start here 
+
+    $(window).on("load",function(){
+        $(".preloader-main").delay(2000).fadeOut(500)
+    })
+
+    // mixit up operation 
+
+    var mixer = mixitup(".fitures", {
+        selectors: {
+            target: '.mix'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+
+    // slick slider part start here 
+
+    $('.testi-wrapper').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots:true,
+      });
+    
+})
+
+
